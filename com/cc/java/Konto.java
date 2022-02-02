9941689ff88c68e26d76d3d23b4344ad232b08c7
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-02-02 09:36:16 +0100

    Add files via upload

---
+++ com/cc/java/Konto.java
@@ -0,0 +1,21 @@
+package com.cc.java;
+
+public class Konto {
+    
+    private int kontostand;
+
+    public Konto(int kontostand) {
+       this.kontostand = kontostand;
+    }
+ 
+    // Getter
+    public int getKontostand() {
+       return kontostand;
+    }
+ 
+    // eine Art Setter ...
+    public void updateKontostand(int factor) {
+       this.kontostand *= factor;
+    }
+
+}
